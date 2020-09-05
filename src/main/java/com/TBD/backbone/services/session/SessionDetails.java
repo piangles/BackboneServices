76edb8e9e692135b805a2bd28d4a32c51129cd9b
Saradhi Vemuri <saradhivs@yahoo.com>
2020-09-05 11:07:57 -0400

    Refactored

--- src/main/java/com/TBD/backbone/services/session/SessionDetails.java
+++ src/main/java/com/TBD/backbone/services/session/SessionDetails.java
@@ -1,20 +1,54 @@
 package com.TBD.backbone.services.session;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class SessionDetails implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	
+	private String userId = null;
 	private String sessionId = null;
+	private Date createdTS = null;
+	private Date lastAccessedTS = null;
 	
-	public SessionDetails(String sessionId)
+	public SessionDetails(String userId, String sessionId)
 	{
+		super();
+		this.userId = userId;
 		this.sessionId = sessionId;
+		this.createdTS = new Date();
+		this.lastAccessedTS = createdTS;
 	}
-	
+
+	public String getUserId()
+	{
+		return userId;
+	}
+
 	public String getSessionId()
 	{
 		return sessionId;
+	}
+
+	public Date getCreatedTS()
+	{
+		return createdTS;
+	}
+
+	public Date getLastAccessedTS()
+	{
+		return lastAccessedTS;
+	}
+	
+	public void touch()
+	{
+		this.lastAccessedTS = new Date();
+	}
+
+	@Override
+	public String toString()
+	{
+		return "SessionDetails [userId=" + userId + ", sessionId=" + sessionId + ", createdTS=" + createdTS + ", lastAccessedTS=" + lastAccessedTS + "]";
 	}
 }
