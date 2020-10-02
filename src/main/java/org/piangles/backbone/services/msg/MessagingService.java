786282b0ea4d44516a7e15280e8c5f1fec3e3f20
saradhivs <saradhivs@yahoo.com>
2020-10-02 14:24:51 -0400

    Renamed endpoint.

--- src/main/java/org/piangles/backbone/services/msg/MessagingService.java
+++ src/main/java/org/piangles/backbone/services/msg/MessagingService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface MessagingService
 {
-	public List<Topic> getTopicsFor(String userId) throws MessagingException;
+	public List<Topic> getTopicsForUser(String userId) throws MessagingException;
 	public List<Topic> getTopicsForAliases(List<String> aliases) throws MessagingException;
 	public void fanOut(FanoutRequest fanoutRequest) throws MessagingException;
 }
