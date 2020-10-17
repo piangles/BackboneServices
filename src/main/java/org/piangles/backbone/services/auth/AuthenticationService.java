4040798f473b1af3040f74812b98bd3ecf109836
saradhivs <saradhivs@yahoo.com>
2020-10-17 17:59:09 -0400

    Added a comment.

--- src/main/java/org/piangles/backbone/services/auth/AuthenticationService.java
+++ src/main/java/org/piangles/backbone/services/auth/AuthenticationService.java
@@ -6,9 +6,12 @@ public interface AuthenticationService
 	
 	public AuthenticationResponse authenticate(Credential credential) throws AuthenticationException;
 
-	public AuthenticationResponse changePassword(String userId, String oldPassword, String newPassword) throws AuthenticationException;
-
 	public void generateResetToken(String loginId) throws AuthenticationException;
 	
 	public AuthenticationResponse validatePasswordStrength(String password) throws AuthenticationException;
+
+	/**
+	 * This is the only call in this service that needs a valid session
+	 */
+	public AuthenticationResponse changePassword(String userId, String oldPassword, String newPassword) throws AuthenticationException;
 }
