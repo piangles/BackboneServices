5a7b3f7ebb36dea1cdc90f042a00a163ce116992
saradhivs <saradhivs@yahoo.com>
2020-10-17 17:39:24 -0400

    Removed Setter for Properties.

--- src/main/java/org/piangles/backbone/services/prefs/UserPreference.java
+++ src/main/java/org/piangles/backbone/services/prefs/UserPreference.java
@@ -8,37 +8,37 @@ public final class UserPreference implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private Properties nvPair = null;
-	
+
 	public UserPreference()
 	{
 		this.nvPair = new Properties();
 	}
-	
+
+	public UserPreference(Properties properties)
+	{
+		this.nvPair = properties;
+	}
+
 	public String getValue(String name)
 	{
 		return nvPair.getProperty(name);
 	}
-	
+
 	public void setValue(String name, String value)
 	{
 		nvPair.setProperty(name, value);
 	}
-	
+
 	public Properties getProperties()
 	{
 		return nvPair;
-	}
-
-	public void setProperties(Properties properties)
-	{
-		this.nvPair = properties;
 	}
 
 	public boolean isEmpty()
 	{
 		return nvPair.isEmpty();
 	}
-	
+
 	@Override
 	public String toString()
 	{
