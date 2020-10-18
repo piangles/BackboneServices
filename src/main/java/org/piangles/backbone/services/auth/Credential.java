254882bf0c59fa73a65d4e111e9c0aa6bdf57719
saradhivs <saradhivs@yahoo.com>
2020-10-17 20:14:43 -0400

    Made it serializable.

--- src/main/java/org/piangles/backbone/services/auth/Credential.java
+++ src/main/java/org/piangles/backbone/services/auth/Credential.java
@@ -1,7 +1,11 @@
 package org.piangles.backbone.services.auth;
 
-public final class Credential
+import java.io.Serializable;
+
+public final class Credential implements Serializable
 {
+	private static final long serialVersionUID = 1L;
+	
 	private String loginId = null;
 	private String password = null;
 	
