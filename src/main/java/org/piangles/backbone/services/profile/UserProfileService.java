7c180526f148d9eaf93778b0de19c6d6cbdeea32
saradhivs <saradhivs@yahoo.com>
2020-12-10 06:59:36 -0500

    Added comments.

--- src/main/java/org/piangles/backbone/services/profile/UserProfileService.java
+++ src/main/java/org/piangles/backbone/services/profile/UserProfileService.java
@@ -6,13 +6,13 @@ public interface UserProfileService
 	
 	/**
 	 * Returns a userId which is system generated.
-	 *  
-	 * @param profile
-	 * @return
-	 * @throws UserProfileException
 	 */
 	public String createProfile(BasicUserProfile profile) throws UserProfileException;
 	public String searchProfile(BasicUserProfile profile) throws UserProfileException;
+	
+	/**
+	 * These are the only calls in this service that needs a valid session
+	 */
 	public BasicUserProfile getProfile(String userId) throws UserProfileException;
 	public void updateProfile(String userId, BasicUserProfile profile) throws UserProfileException;
 }
