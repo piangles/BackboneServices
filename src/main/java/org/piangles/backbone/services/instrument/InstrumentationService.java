9aa3362f1560cfd95529ffbf12bdcfc443e919c7
saradhivs <saradhivs@yahoo.com>
2021-01-13 07:55:05 -0500

    Added two new services.

---
+++ src/main/java/org/piangles/backbone/services/instrument/InstrumentationService.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ 
+package org.piangles.backbone.services.instrument;
+
+import org.piangles.core.util.instrument.Measures;
+
+public interface InstrumentationService
+{
+	public static final String NAME = InstrumentationService.class.getSimpleName();
+	
+	public void record(Measures measures);
+}
