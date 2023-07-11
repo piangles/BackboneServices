b0a1bd8be694cf6302d1ae28c284f3f162e5ebce
Budhaditya Biswas <66356777+biswasaditya-simform@users.noreply.github.com>
2023-07-11 19:26:20 +0530

    add new method to FeatureToggleService (#5)

---
+++ src/main/java/org/piangles/backbone/services/feature/UpdateFeatureRequest.java
@@ -0,0 +1,28 @@
+package org.piangles.backbone.services.feature;
+
+import java.io.Serializable;
+
+public final class UpdateFeatureRequest implements Serializable {
+
+  private final String groupId;
+  private final String featureId;
+  private final boolean enabled;
+
+  public UpdateFeatureRequest(String groupId, String featureId, boolean enabled) {
+    this.groupId = groupId;
+    this.featureId = featureId;
+    this.enabled = enabled;
+  }
+
+  public String getGroupId() {
+    return groupId;
+  }
+
+  public String getFeatureId() {
+    return featureId;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+}
